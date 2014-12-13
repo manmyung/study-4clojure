0d73b3c2316c9e6cd0c245ea0a2996be5853f9e0
manmyung <manmyung@gmail.com>
2014-12-13 20:03:35 +0900

    p13

---
+++ src/study_4clojure/p13_answers.clj
@@ -0,0 +1,10 @@
+;; 4Clojure Question 13
+;;
+;; The rest function will return all the items of a sequence except the first.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+(= '(20 30 40) (rest [10 20 30 40]))
+
+(rest [10 20 30 40]) ;(20 30 40) 벡터의 경우 list 로 나오네.
+(rest {10 20 30 40 50 60});([30 40] [10 20]) 맵의 경우 벡터의 list로 나오네.
---
+++ src/study_4clojure/p13_question.clj
@@ -0,0 +1,7 @@
+;; 4Clojure Question 13
+;;
+;; The rest function will return all the items of a sequence except the first.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+(= __ (rest [10 20 30 40]))
