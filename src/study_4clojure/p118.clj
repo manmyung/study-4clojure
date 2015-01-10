20cadb356c5c47a6f7abf9714c3294f033be1bc6
manmyung <manmyung@gmail.com>
2015-01-10 16:17:12 +0900

    ~p118

--- src/study_4clojure/p118.clj
+++ src/study_4clojure/p118.clj
@@ -7,7 +7,10 @@
 ;; Use M-x 4clojure-check-answers when you're done!
 
 (= [3 4 5 6 7]
-   (__ inc [2 3 4 5 6]))
+   ((fn k [f col]
+      (if (empty? col)
+        col
+        (cons (f (first col)) (lazy-seq (k f (rest col)))))) inc [2 3 4 5 6]))
 
 (= (repeat 10 nil)
    (__ (fn [_] nil) (range 10)))
@@ -16,3 +19,33 @@
    (->> (__ inc (range))
         (drop (dec 1000000))
         (take 2)))
+
+(
+(fn k [f col]
+  (if (empty? col)
+    col
+    (cons (f (first col)) (lazy-seq (k f (rest col))))))
+  inc [2 3 4 5 6])
+
+;지금. 이전이랑 거의 똑같네.
+(fn k [f col]
+  (if (empty? col)
+    col
+    (cons (f (first col)) (lazy-seq (k f (rest col))))))
+
+;이전
+(fn _map [f col]
+  (if (empty? col)
+    []
+    (cons (f (first col)) (lazy-seq (_map f (rest col))))))
+
+;max. 여기서 배울 점 deconstructuring.
+(fn m [f [h & t :as v]]
+  (if (empty? v)
+    ()
+    (lazy-seq (cons (f h) (m f t)))))
+
+;chouser. 여기서 배울 점 if를 lazy-seq 안에서 쓸 수 있다는 점.
+(fn l [f [a & m]]
+  (lazy-seq
+    (cons (f a) (if m (l f m)))))
