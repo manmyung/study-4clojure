5725c3a883042cca8276a799f87cc24453e50c00
manmyung <manmyung@gmail.com>
2015-01-10 17:25:25 +0900

    ~p95

--- src/study_4clojure/p95.clj
+++ src/study_4clojure/p95.clj
@@ -4,10 +4,28 @@
 ;;
 ;; Use M-x 4clojure-check-answers when you're done!
 
-(= (__ '(:a (:b nil nil) nil))
+(= ((fn k [x]
+      (if (coll? x)
+        (let [[a b c] x]
+          (if (and (= 3 (count x))
+                   (k a)
+                   (k b)
+                   (k c))
+            true
+            false))
+        (not (false? x)))) '(:a (:b nil nil) nil))
    true)
 
-(= (__ '(:a (:b nil nil)))
+(= ((fn k [x]
+      (if (coll? x)
+        (let [[a b c] x]
+          (if (and (= 3 (count x))
+                   (k a)
+                   (k b)
+                   (k c))
+            true
+            false))
+        (not (false? x)))) '(:a (:b nil nil)))
    false)
 
 (= (__ [1 nil [2 [3 nil nil] [4 nil nil]]])
@@ -24,3 +42,40 @@
 
 (= (__ '(:a nil ()))
    false)
+
+;이전
+(fn _tree
+  ([] false)
+  ([x]
+    (if (coll? x)
+      (apply _tree x)
+      (not= x false)))
+  ([x y] false)
+  ([x y z] (and (_tree x) (_tree y) (_tree z)))
+  ([x y z & more] false)
+  )
+
+;지금. 이전보다는 좀더 깔끔.
+(fn k [x]
+  (if (coll? x)
+    (let [[a b c] x]
+      (if (and (= 3 (count x))
+               (k a)
+               (k b)
+               (k c))
+        true
+        false))
+    (not (false? x))))
+
+;max
+(fn ? [t]
+  (or (and (coll? t)
+           (= (count t) 3)
+           (every? ? (rest t)))
+      (nil? t)))
+;나와 차이점
+;- leaf node에 대해서 nil?로만 체크
+;- every? 사용
+;- every? 에 rest만 체크
+;- if 대신 or 사용. 이건 생각하기 쉽진 않다.
+;결론: 이 문제에 대해서는 max가 깔끔하게 구현했다. every? 기억하자.
