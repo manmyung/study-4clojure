d352113f30a1f2d85c3df460c8a6e76448a14b93
manmyung <manmyung@gmail.com>
2015-01-17 08:23:25 +0900

    ~p60

--- src/study_4clojure/p60.clj
+++ src/study_4clojure/p60.clj
@@ -11,3 +11,32 @@
 (= (__ conj [1] [2 3 4]) [[1] [1 2] [1 2 3] [1 2 3 4]])
 
 (= (last (__ * 2 [3 4 5])) (reduce * 2 [3 4 5]) 120)
+
+;처음 답. 오래 걸렸다. 겨우겨우 만들었다. recursive하게 생각하기가 힘들다.
+(fn
+  ([f c]
+    (for [i (map inc (range))
+          :let [sc (take i c)]
+          :while (not-empty (drop (dec i) c))]
+      (reduce f sc)))
+  ([f v c]
+    (for [i (range)
+          :let [sc (take i c)]
+          :while (not-empty (drop (dec i) c))]
+      (reduce f v sc))))
+
+;다음 답. lazy-seq를 사용할 수 있다는 것을 알고 난 후. 결국 (f v x) 를 v 자리에 넣는 것이 핵심.
+(fn r
+  ([f [x & xs]]
+    (cons x (lazy-seq (when (first xs)
+                        (r f (f x (first xs)) (rest xs))))))
+  ([f v [x & xs]]
+    (cons v (lazy-seq (when x
+                        (r f (f v x) xs))))))
+
+;max. 함수 선언이 뒤에 있어도 사용가능.
+(fn g
+  ([f [x & s]] (g f x s))
+  ([f a [x & s]]
+    (lazy-seq
+      (cons a (if x (g f (f a x) s))))))
