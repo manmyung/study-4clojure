b5b63d2a9cb9ebb8e745665f05e6880cc41e91d6
manmyung <manmyung@gmail.com>
2015-03-25 20:37:37 +0900

    ~p12,15,16,18

---
+++ src/study_4clojure/p12.clj
@@ -0,0 +1,18 @@
+;; 4Clojure Question 12
+;;
+;; All Clojure collections support sequencing.  You can operate on sequences with functions like first, second, and last.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+(= __ (first '(3 2 1)))
+
+(= __ (second [2 3 4]))
+
+(= __ (last (list 1 2 3)))
+
+;me
+(= 3 (first '(3 2 1)))
+
+(= 3 (second [2 3 4]))
+
+(= 3 (last (list 1 2 3)))
