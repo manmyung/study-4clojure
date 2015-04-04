9b7a5b111190a6e01157853029c6df60ea133c55
manmyung <manmyung@gmail.com>
2015-04-04 09:11:53 +0900

    ~p53

--- src/hard.clj
+++ src/hard.clj
@@ -11,3 +11,48 @@
 (= (__ [2 3 3 4 5]) [3 4 5])
 
 (= (__ [7 6 5 4]) [])
+
+;me
+(fn [l]
+  (->>
+    (reduce
+    #(let [last-l (last %1)
+           last-e (last last-l)]
+      (if (or (nil? last-e) (>= last-e %2))
+        (conj %1 [%2])
+        (conj (vec (butlast %1)) (conj last-l %2))))
+    [] l)
+    (reduce #(if (< (count %1) (count %2)) %2 %1))
+    ((fn [x] (if (< 1 (count x)) x [])))))
+
+;daowen
+#(loop [xs %, best-c 0, best-s '()]
+  (if (< (count xs) 2) best-s
+                       (let [s (map second (take-while (fn [p] (< (first p) (second p))) (partition 2 1 xs)))
+                             ss (if (empty? s) s (cons (first xs) s))
+                             c (count ss)
+                             [next-c next-s] (if (> c best-c) [c ss] [best-c best-s])]
+                         (recur (rest xs) next-c next-s))))
+
+;hypirion
+(fn [coll]
+  (->>
+    (range 2 (inc (count coll)))
+    (mapcat #(partition % 1 coll))
+    (filter #(apply < %))
+    (cons [])
+    (sort-by count >)
+    first))
+
+;chouser
+#(or
+  (first
+    (sort-by
+      (comp - count)
+      (for [f [(fn [[a b]] (= b (+ 1 a)))]
+            p (partition-by f (map list % (next %)))
+            r [`[~@(map first p) ~(last (last p))]]
+            :when (f r)]
+        r)))
+  [])
+
