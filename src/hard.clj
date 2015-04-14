4e54423b9e9d67b0460e1f767c75e3dbdd565755
manmyung <manmyung@gmail.com>
2015-04-15 02:06:11 +0900

    ~p82

--- src/hard.clj
+++ src/hard.clj
@@ -279,3 +279,49 @@
 (= true (__ #{"share" "hares" "shares" "hare" "are"}))
 
 (= false (__ #{"share" "hares" "hare" "are"}))
+
+;me
+(fn [s]
+  (let [diff (fn diff [s1 s2]
+               (cond
+                 (empty? s1) (count s2)
+                 (empty? s2) (count s1)
+                 :else
+                 (min
+                   (+ (if (= (first s1) (first s2)) 0 1)
+                      (diff (rest s1) (rest s2)))
+                   (+ 1 (diff (rest s1) s2))
+                   (+ 1 (diff s1 (rest s2))))))
+        relative (into
+                   (reduce #(into %1 {%2 (keep (fn [x] (if (= 1 (diff %2 x)) x nil)) s)}) {} s)
+                   {nil (seq s)})
+        search (fn search [path visited]
+                 (let [last-chains (relative (last path))
+                       last-chains-except (remove #(visited %) last-chains)]
+                   (if (empty? last-chains-except)
+                     [path]
+                     (mapcat #(search (conj path %) (conj visited %)) last-chains-except))))
+        n (count s)]
+    (true? (some #(= n (count %)) (search [] #{})))
+    ))
+
+;silverio.
+#(let [
+       nv    (count %)
+       words (map seq %)
+       edge? (fn edge? [w1 w2]
+               (let [[h1 & t1] w1 [h2 & t2] w2]
+                 (if (= h1 h2) (edge? t1 t2)
+                               (or (= t1 t2)     ; substitution
+                                   (= t1 w2)     ; deletion
+                                   (= w1 t2))))) ; insertion
+       graph (apply (partial merge-with concat) (for [a words b words
+                                                      :when (and (not= a b) (edge? a b))] {a [b]}))
+       chain (fn chain [visited w] ; depth-first search for the chain
+               (or (= nv (inc (count visited)))
+                   (some (partial chain (conj visited w))
+                         (filter (comp not visited) (graph w)))))]
+  (not (not-any? (partial chain #{}) (keys graph))))
+; edge? 를 지금 상황에 딱 적합하게 작성했음.
+; graph 만드는 코드가 이해가 쉽게 잘 되어 있다. for로 돌리고 merge-with concat 모으기.
+; chain도 모든 path를 다 만드는 것이 아니라 만족하면 true 리턴. 이 chain은 정말 내가 만들고 싶었던 코드이다.
